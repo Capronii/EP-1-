8cf93cb16dcfe992d3836f8318799de50366ebd7
Capronii <gcapronii@hotmail.com>
2020-03-31 13:51:45 -0300

    EP

---
+++ ep.py
@@ -0,0 +1,36 @@
+for random import randint
+din=100
+print('bem vindo ao Craps')
+'''Criar print com as regras depois'''
+
+def valor_aposta():
+    aposta=int(input("Quanto quer apostar"))
+    return
+
+def aposta_valida(din,aposta):
+    if aposta<=din:
+        return True
+    else:
+        return False
+
+
+
+     
+
+
+def jogo():
+    
+    while aposta_valida(din,aposta)==False :
+        print('erro aposta maior que saldo')
+        aposta=valor_aposta()
+
+    while aposta_valide(din,aposta)==True
+        dado1=random.randint(1,6)
+        dado2=random.randint(1,6)
+        print('Dado 1:',dado1)
+        print('Dado 2:'.dado2)
+        dado=int(dado1)+int(dado2)
+        print('Soma dos dados:',dado)
+
+        d
+
